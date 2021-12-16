e7403b1489ef59608b294c868e261dd5a41f7d9b
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2021-12-16 12:26:42 -0700

    Task ( async-await)-3

---
+++ TextWebApp.API/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace TextWebApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetContentAsync()
+        {
+            var myTask = new HttpClient().GetStringAsync("https://www.google.com");
+
+            var data = await myTask;
+
+            return Ok(data);
+
+        }
+
+    }
+}
