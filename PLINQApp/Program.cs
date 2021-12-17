73fd839776145949b366149ef581d5e174af1900
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2021-12-17 08:32:04 -0700

    WithExecuteMode nedir ?

--- PLINQApp/Program.cs
+++ PLINQApp/Program.cs
@@ -12,22 +12,31 @@ namespace PLINQApp
             AdventureWorks2017Context context = new AdventureWorks2017Context();
 
             //var product = (from p in context.Products.AsParallel()
-            //    where p.ListPrice > 10M
-            //    select p).Take(10);
+            //               where p.ListPrice > 10M
+            //               select p).Take(10);
+
+            //var product2 = context.Products.AsParallel().Where(p => p.ListPrice > 10M).Take(10);
 
             //product.ForAll(x =>
             //{
             //    Console.WriteLine(x.Name);
             //});
 
-            var product2 = (from p in context.Products
-                where p.ListPrice > 10M
-                select p).Take(10);
 
-            product2.ToList().ForEach(x =>
+            context.Products.AsParallel().WithExecutionMode(ParallelExecutionMode.ForceParallelism).ForAll(p =>
             {
-                Console.WriteLine(x.Name);
+                Console.WriteLine(p.Name);
             });
+
+
+            //var product2 = (from p in context.Products
+            //    where p.ListPrice > 10M
+            //    select p).Take(10);
+
+            //product2.ToList().ForEach(x =>
+            //{
+            //    Console.WriteLine(x.Name);
+            //});
 
         }
     }
