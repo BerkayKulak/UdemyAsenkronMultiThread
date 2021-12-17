f3d38cf36ce75ed44eac026c29b02055451f48fe
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2021-12-17 07:19:11 -0700

    AsParallel()

---
+++ PLINQApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace PLINQApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = Enumerable.Range(1, 500).ToList();
+
+            var newArray = array.AsParallel().Where(x => x % 2 == 0);
+
+            newArray.ToList().ForEach(x =>
+            {
+                Console.WriteLine(x);
+            });
+        }
+    }
+}
