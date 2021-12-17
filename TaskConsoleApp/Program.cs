b72699eb7474fccd759116d5d9128aabfc04a3c4
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2021-12-17 04:01:40 -0700

    Task Akış durumu

--- TaskConsoleApp/Program.cs
+++ TaskConsoleApp/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net.Http;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -11,18 +12,25 @@ namespace TaskConsoleApp
 
     class Program
     {
-        public static int CacheData { get; set; } = 150;
-        
         private async static Task Main(string[] args)
         {
+            Console.WriteLine("1. adım");
 
-            await GetData();
+            var task = GetContent();
+
+            Console.WriteLine("2. adım");
+
+            var content =   await task;
+
+            Console.WriteLine("3. adım" + content.Length);
+
         }
 
-        public static ValueTask<int> GetData()
+        public static async Task<string> GetContent()
         {
-            return new ValueTask<int>(CacheData);
-        }
+            var content = await new HttpClient().GetStringAsync("https://www.google.com");
 
+            return content;
+        }
     }
 }
